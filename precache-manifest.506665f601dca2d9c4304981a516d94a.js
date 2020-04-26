c2e340378f166483b7b91c896944975bcdccdc0b
skibasu <lukasz.skiba@gogomedia.pl>
2020-04-26 15:39:51 +0200

    Updates

---
+++ precache-manifest.506665f601dca2d9c4304981a516d94a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed171d2f65b90df7a66190212789064",
+    "url": "skibasu.github.io/mychords/index.html"
+  },
+  {
+    "revision": "7a44e274f9abecbf9e26",
+    "url": "skibasu.github.io/mychords/static/css/main.a455e810.chunk.css"
+  },
+  {
+    "revision": "b7b661ff9b23e89ee35f",
+    "url": "skibasu.github.io/mychords/static/js/2.002ee84d.chunk.js"
+  },
+  {
+    "revision": "7a44e274f9abecbf9e26",
+    "url": "skibasu.github.io/mychords/static/js/main.e764b143.chunk.js"
+  },
+  {
+    "revision": "6908cb5ef5eea11467f5",
+    "url": "skibasu.github.io/mychords/static/js/runtime-main.9521012a.js"
+  },
+  {
+    "revision": "5aa490544f3f632f9c837d23487d54dc",
+    "url": "skibasu.github.io/mychords/static/media/checked-black.5aa49054.svg"
+  },
+  {
+    "revision": "b126a1ffd882a1092d12976f7842c6de",
+    "url": "skibasu.github.io/mychords/static/media/logo.b126a1ff.png"
+  },
+  {
+    "revision": "711ec229ce8037b347da0bc178b5f689",
+    "url": "skibasu.github.io/mychords/static/media/piano.711ec229.jpg"
+  },
+  {
+    "revision": "b75b4feb916c1596982fda150fd94188",
+    "url": "skibasu.github.io/mychords/static/media/piano.b75b4feb.svg"
+  }
+]);
--- src/utils/chordsGenerators.js
+++
@@ -1,5 +0,0 @@
-import { createChords } from "./createChords";
-
-export const chords = ({ hash, flats }, rules = { chordType: "major" }) => {
-    return [createChords(flats, rules, 'flats'), createChords(hash, rules, 'hash')];
-};
